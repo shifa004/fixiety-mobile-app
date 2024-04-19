ced56df4aba161b6ade44ff93f439d6df0fefd3f
shifa004 <135617151+shifa004@users.noreply.github.com>
2024-04-19 14:59:08 +0300

    worked on journal

--- components/JournalCalendar.js
+++ components/JournalCalendar.js
@@ -1,47 +1,82 @@
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, View, Dimensions, TouchableOpacity, ScrollView } from 'react-native'
 import React, {useState, useEffect} from 'react'
-import {doc, setDoc, addDoc, collection} from "firebase/firestore";
+import {doc, setDoc, getDocs, addDoc, collection} from "firebase/firestore";
 import { db } from './config';
+import {Card, Avatar } from '@rneui/themed';
+import { MaterialCommunityIcons} from 'react-native-vector-icons';
 
+const screenWidth = Dimensions.get('window').width
+const screenHeight = Dimensions.get('window').height
 
-const JournalCalendar = ({route, navigation}) => {
-  const getCurrentDate=()=>{
- 
+const JournalCalendar = ({navigation}) => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    readAll()
+    return () => {}
+  }, [])
+
+  const readAll = async () => {
+    const docs = await getDocs(collection(db, "entries"));
+    const temp = [...entries]
+    docs.forEach((doc) => {
+      console.log(doc.id)
+      console.log(doc.data())
+      temp.push(doc.data())     
+    });
+    temp.sort((a, b) => parseDate(a.date) - parseDate(b.date));
+    setEntries([...temp]);
+
+  } 
+
+  const getCurrentDate=()=>{ 
     var date = new Date().getDate();
     var month = new Date().getMonth() + 1;
     var year = new Date().getFullYear();
-
-    //Alert.alert(date + '-' + month + '-' + year);
-    // You can turn it in to your desired format
-    return date + '-' + month + '-' + year;//format: d-m-y;
+    return date + '-' + month + '-' + year;
   }
 
-  useEffect(() => {
-    async function addEntry() {
-      if (route.params?.entry) {
-        const currentDate = getCurrentDate();
-        try {
-          const docRef = await addDoc(collection(db, "entries"), {
-            date: currentDate,
-            score: route.params.score,
-            feel: route.params.feel,
-            entry: route.params.entry,
-          }, {merge: true});
-          console.log("Document written with ID: ", docRef.id);
-        } catch (error) {
-          console.error("Error adding document: ", error);
-        }
-      }
-    }
+  function parseDate(dateStr) {
+    const [day, month, year] = dateStr.split('-').map(Number);
+    return new Date(year, month - 1, day);
+  }
 
-    // Call the async function
-    addEntry();
-  }, [route.params?.entry]);
+  const emojis = [
+    { name: 'emoticon-happy-outline', label: 'Happy', color: "green" },
+    { name: 'emoticon-neutral-outline', label: 'Neutral', color: "grey" },
+    { name: 'emoticon-sad-outline', label: 'Sad', color: "red"},
+  ];
 
+  const findEmojiByLabel = (label) => {
+    return emojis.find(emoji => emoji.label.toLowerCase() === label.toLowerCase());
+  };
   return (
-    <SafeAreaView style={styles.container}>
-     
-      
+    <SafeAreaView style ={styles.container}>
+      {console.log(entries)}
+      <ScrollView contentContainerStyle ={styles.all}>        
+        {(entries.map((x, i) => {
+          console.log(entries)
+          const e = findEmojiByLabel(x.feel);
+          return (
+          <TouchableOpacity key={i} onPress={() => navigation.navigate('Entry', {e: x})}>
+            <Card containerStyle={styles.entry} >
+              <View style={styles.fields}>
+                <Text style={styles.date}>{x.date}</Text>
+              </View>
+                <View style={styles.content}>
+                <View>
+                  <MaterialCommunityIcons name={e.name}  color={e.color} size={60}/> 
+                </View>
+                <View style={styles.score}>
+                  <Text style={styles.scr}>Anxiety Score: {x.score}</Text>
+                </View>
+              </View>
+            </Card>
+          </TouchableOpacity>
+          );      
+        }))
+      }
+      </ScrollView>
     </SafeAreaView>
   )
 }
@@ -52,6 +87,31 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     justifyContent: 'center',
-    alignItems: 'center'
+  },
+  fields: {
+    alignItems:'center',
+  },
+  date: {
+    fontSize: screenWidth*0.05,
+  },
+  entry: {
+    borderRadius: 10,
+    margin: screenHeight*0.02,
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    backgroundColor: "white",
+    padding: screenHeight*0.02,
+  },
+  content: {
+    flexDirection:'row',
+    justifyContent: 'space-evenly'
+  },
+  score: {
+    justifyContent: 'center',
+  },
+  scr: {
+    fontSize: screenWidth*0.04
   }
 })
--- components/config.js
+++ components/config.js
@@ -1,10 +1,15 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+// import {getAuth, initializeAuth, getReactNativePersistence} from 'firebase/auth';
+// import {getFirestore} from 'firebase/firestore';
+// import { getStorage } from "firebase/storage";
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import {getAuth, initializeAuth, getReactNativePersistence} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
@@ -19,9 +24,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-  });
-
-export const db = getFirestore(app);
+export const db = getFirestore(app)
+export const auth = getAuth(app)
 export const storage = getStorage(app);
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = initializeAuth(app, {
+//     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+//   });
+
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
